df0aa5629ecdc526029ec4167db9dec6b6770ca0
Danilo Araújo <daniloaraujo1990@hotmail.com>
2022-05-05 15:25:43 -0300

    NUnit Test

---
+++ Teste/Contato.DAO.Test/ContatoTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Agenda.Domain;
+
+namespace Agenda.DAO.Test
+{
+    [TestFixture]
+    public class ContatoTest
+    {
+        Contatos contatos;
+
+        [SetUp]
+        public void Setup()
+        {
+            contatos = new Contatos();
+        }
+
+        [Test]
+        public void InserirTest()
+        {
+            // Monta
+            Contato contato = new Contato()
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Joao"
+            };
+            // Executa
+            int ret = contatos.Inserir(contato);
+            // Verifica
+            Assert.Greater(ret, 0);
+        }
+
+        [Test]
+        public void ObterTest()
+        {
+            //string id = "4D7A6C9C-4FF4-49F2-9E13-FA1145A4B270";
+            //string nome = "Danilo";
+            //id = Guid.NewGuid().ToString();
+            Contato contato = new Contato()
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Mariana"
+            };
+            Contato contatoRetornado = new Contato();
+            contatos.Inserir(contato);
+            contatoRetornado =  contatos.Obter(contato.Id.ToString());
+
+            Assert.AreEqual(contato.Id, contatoRetornado.Id);
+            Assert.AreEqual(contato.Nome, contatoRetornado.Nome);
+        }
+
+        [Test]
+        public void ListarTest()
+        {
+            List<Contato> listaContatos = new List<Contato>();
+             listaContatos = contatos.Listar();
+            Assert.Greater(listaContatos.Count, 0);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            contatos = null;
+        }
+    }
+}
---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Danilo";
+            string id = Guid.NewGuid().ToString();
+            string nomeRecuperado = string.Empty;
+
+            string stringConexao = @"Data Source = .\MSSQLSERVER02; Initial Catalog = Agenda; Integrated Security = True;";
+
+            SqlConnection conexao = new SqlConnection(stringConexao);
+            
+            conexao.Open();
+
+            //string sql = string.Format("INSERT INTO Contato (Id, Nome) VALUES ({0}, {1})", id, nome);
+            string sql = $"INSERT INTO Contato(Id,Nome)VALUES('{id}', '{nome}')";
+            SqlCommand cmd = new SqlCommand(sql,conexao);
+
+            int retorno = cmd.ExecuteNonQuery();
+            Console.WriteLine($"Numero de linhas afetadas: {retorno}.");
+
+            sql = $"SELECT NOME FROM Contato WHERE ID='{id}'";
+
+            cmd = new SqlCommand(sql,conexao);
+
+            object ret = cmd.ExecuteScalar();
+            nomeRecuperado = ret == null ? string.Empty : ret.ToString();
+            Console.WriteLine($"Nome recuperado: {nomeRecuperado}");
+
+            conexao.Close();
+        }
+    }
+}
