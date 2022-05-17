4065252917b33b99573aae01ca71b4027399bdf2
Danilo Araújo <daniloaraujo1990@hotmail.com>
2022-05-17 17:45:34 -0300

    Entity Framework
    Studies with Entity Framework

---
+++ CrossPlatform/RecifeUniversity/RecifeUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecifeUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
