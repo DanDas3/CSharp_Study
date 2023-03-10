5c563077a87a0e90ecb19e980e743664022e3483
Danilo Araújo <daniloaraujo1990@hotmail.com>
2023-03-10 12:55:17 -0300

    Add Entity study
    Adding Entity study project with .gitignore files

---
+++ CrossPlatform/EntityTest/EntityTest/Context/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EntityTest.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityTest.Context
+{
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
